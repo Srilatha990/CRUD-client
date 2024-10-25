21e72749f7c7f4f907652a292f0fa277107515d5
Srilatha990 <162280646+Srilatha990@users.noreply.github.com>
2024-10-25 17:25:52 +0530

    Update details.jsx

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -10,7 +10,7 @@ function Details() {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/users/${id}`);
+        const response = await axios.get(`https://crud-server-80x5.onrender.com/api/users/${id}`);
         setUser(response.data);
       } catch (error) {
         console.error('Error fetching user details:', error.response ? error.response.data : error.message);
